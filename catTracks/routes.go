5ed2eccef2dabaa2fbce3db229de2b257893e0c8
jlanej <jlanej@gmail.com>
2017-01-08 13:39:08 -0600

    routies

---
+++ catTracks/routes.go
@@ -0,0 +1,22 @@
+package catTracks
+
+import "net/http"
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		indexHandler,
+	},
+
+}
