1f7d68470a56b72c5d6ae86652368b24ce72748a
Mr. Is <isaac.ardis@gmail.com>
2017-01-20 17:49:03 +0100

    opt delete spain

--- catTracks/models.go
+++ catTracks/models.go
@@ -119,6 +119,27 @@ func DeleteTestes() error {
 	return e
 }
 
+// DeleteSpain deletes spain
+func DeleteSpain() error {
+	e := GetDB().Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("tracks"))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			var tp trackPoint.TrackPoint
+			e := json.Unmarshal(v, &tp)
+			if e != nil {
+				fmt.Println("Error deleting testes.")
+				return e
+			}
+			if tp.Lat < 12.0 && tp.Lat > -10.0 {
+				b.Delete(k)
+			}
+		}
+		return nil
+	})
+	return e
+}
+
 //get everthing in the db... can do filtering some other day
 
 //TODO make queryable ala which cat when
--- main.go
+++ main.go
@@ -15,10 +15,12 @@ func main() {
 	var porty int
 	var clearDBTestes bool
 	var testesRun bool
+	var noSpain bool
 
 	flag.IntVar(&porty, "port", 8080, "port to serve and protect")
 	flag.BoolVar(&clearDBTestes, "castrate-first", false, "clear out db of testes prefixed points") //TODO clear only certain values, ie prefixed with testes based on testesRun
 	flag.BoolVar(&testesRun, "testes", false, "testes run prefixes name with testes-")              //hope that's your phone's name
+	flag.BoolVar(&noSpain, "no-spain", false, "remove spanish wanderings")
 	flag.Parse()
 	// Open Bolt DB.
 	// catTracks.InitBoltDB()
@@ -27,6 +29,12 @@ func main() {
 	}
 	if clearDBTestes {
 		e := catTracks.DeleteTestes()
+		if e != nil {
+			log.Println(e)
+		}
+	}
+	if noSpain {
+		e := catTracks.DeleteSpain()
 		if e != nil {
 			log.Println(e)
 		}
