7458179dc1cd62fb38783535ff105efa63f3c53f
ia <isaac.ardis@gmail.com>
2023-12-18 09:01:41 -0700

    tracks db snaps copier
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- cmd-tracksdb-copy-snaps-only/main.go
+++ cmd-tracksdb-copy-snaps-only/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+
 	bolt "go.etcd.io/bbolt"
 )
+
+var snapBucketKey = []byte("catsnaps")
 
 var flagSourceDB = flag.String("source", "catTracks-old.db", "source database")
 var flagTargetDB = flag.String("target", "catTracks-new.db", "target database")
@@ -16,10 +19,40 @@ func main() {
 	log.Println("target", *flagTargetDB)
 
 	// open source db
-	_, err := bolt.Open(*flagSourceDB, 0666, nil)
+	source, err := bolt.Open(*flagSourceDB, 0666, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// defer source.Close()
+	defer source.Close()
 
+	// open target db
+	target, err := bolt.Open(*flagTargetDB, 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer target.Close()
+
+	// copy snaps
+	err = source.View(func(stx *bolt.Tx) error {
+		b := stx.Bucket(snapBucketKey)
+		if b == nil {
+			log.Fatalln("no snaps bucket in source db")
+		}
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			err := target.Update(func(ttx *bolt.Tx) error {
+				b, err := ttx.CreateBucketIfNotExists(snapBucketKey)
+				if err != nil {
+					return err
+				}
+				log.Println("copying", string(k))
+				err = b.Put(k, v)
+				return err
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
