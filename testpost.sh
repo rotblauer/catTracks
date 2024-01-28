cd9a6e0a033abcb569f1e222c6f49a1826d05567
ia <isaac.ardis@gmail.com>
2024-01-28 09:19:11 -0700

    fixup test*sh scripts
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- testpost.sh
+++ testpost.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 curl -X POST \
     -H "Content-Type: application/json" \
     -H "AuthorizationOfCats: mytoken" \
     -d @testdata/trackpoints.json http://localhost:3001/populate/
+
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "AuthorizationOfCats: mytoken" \
+    -d @testdata/lastknown-rye.geojson http://localhost:3001/populate/
+
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "AuthorizationOfCats: mytoken" \
+    -d @testdata/lastknown-ia.geojson http://localhost:3001/populate/
