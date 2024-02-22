ea6a9ecd4cf3bcb6ce01e9631fe60cf7249a2332
ia <isaac.ardis@gmail.com>
2024-02-22 07:15:00 -0700

    rebuild and redeploy cat tracks to freya (she got nuked)
    Signed-off-by: ia <isaac.ardis@gmail.com>

---
+++ reupfuckingfreya.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# At 20240221:20:00 the ~/track.areteh.co/ directory was nuked. Nothing in it except favicons.
+# No cat tracks executable, no cat tracks data.
+# This caused track pushes to break because the 'linux.trackermain' process there
+# is the point of entry for all cat trackers.
+# So until we move to a reliable server, this is what I did to start it working again.
+
+go build -o /tmp/linux.trackermain .
+rsync -avz /tmp/linux.trackermain freya:~/track.areteh.co/
+rsync -avz ./kickstart freya:~/track.areteh.co/
+
+# Make sure `kickstart` script is executable on Freya.
+# She has a cron (trackalivecheckerkeeper) that will regularly restart
+# this process if not already running. Another awesome workaround
+# for living in Freya's fascist world.
